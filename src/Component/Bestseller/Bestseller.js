b1ea858c72061d56167c29e7bff360c826bf597a
Sushant-saud <sushantsaud9804@gmail.com>
2022-11-30 15:57:30 +0530

    new updata

--- src/Component/Bestseller/Bestseller.js
+++ src/Component/Bestseller/Bestseller.js
@@ -6,8 +6,18 @@ import "rsuite/dist/rsuite.min.css";
 import BestSellerItem from '../BestSellerItem/BestSellerItem';
 import Loadmore from '../LoadMore/Loadmore';
 import best from './best.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  
+} from "reactstrap";
 
 function Bestseller() {
+    const [dropdownopen,setdropdownopen]=useState(false);
+    const toggle=()=>setdropdownopen(prevState=>!prevState);
   return (
     <div className='BEST-SELLER'>
       <h3 className='Best-SELLER-heading'>BEST SELLER</h3>
@@ -26,31 +36,18 @@ function Bestseller() {
         </ul>
         
       </div>
-      <Dropdown title="Best-Product" className='dropdown'>
-                <Dropdown.Item as="a" href=
-                "https://ecommerce123a.herokuapp.com/bestseller/all">
-                All
-                </Dropdown.Item>
-                <Dropdown.Item as="a" href=
-                "https://ecommerce123a.herokuapp.com/bestseller/iphone"
-                >
-                  iphone
-                </Dropdown.Item>
-                <Dropdown.Item as="a" href=
-                "https://ecommerce123a.herokuapp.com/mac">
-                mac
-                </Dropdown.Item>
-                <Dropdown.Item as="a" href=
-                "https://ecommerce123a.herokuapp.com/ipod"
-                >
-                  ipod
-                </Dropdown.Item>
-                <Dropdown.Item as="a" href=
-                "https://ecommerce123a.herokuapp.com/accesories"
-                >
-                  Accessorie
-                </Dropdown.Item>
-            </Dropdown>
+
+<ButtonDropdown isOpen={dropdownopen} toggle={toggle} className='dropdown'>
+  <DropdownToggle caret>Menu</DropdownToggle>
+  <DropdownMenu>
+    <DropdownItem tag={Link} to={`/bestseller/all`}>all</DropdownItem>
+    <DropdownItem tag={Link} to={`/bestseller/iphone`}>iphone</DropdownItem>
+    <DropdownItem tag={Link} to={`/bestseller/ipod`}>ipad</DropdownItem>
+    <DropdownItem tag={Link} to={`/bestseller/mac`}>mac</DropdownItem>
+    <DropdownItem tag={Link} to={`/bestseller/accesories`}>accesories</DropdownItem>
+  </DropdownMenu>
+</ButtonDropdown>
+
       <div className='Best-Item'>
         <BestSellerItem />
       </div>
